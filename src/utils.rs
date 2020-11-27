8caf522a9b2f0f3bf36f55dce645a5ec379c8e4b
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-11-26 21:12:07 -0600

    Split out arrays from fn body

--- src/utils.rs
+++ src/utils.rs
@@ -4,6 +4,65 @@ use std::env;
 
 use crate::models::Response;
 
+const DAYS_OF_WEEK: [&str; 7] = [
+    "tacakuce",
+    "tvcakuce enhayvtke",
+    "mvnte enhayvtke",
+    "ennvrkvpv",
+    "ennvrkvpv enhayvtke",
+    "okkoskv nettv",
+    "tacakcuse",
+];
+
+const MONTHS: [&str; 12] = [
+    "rvfocuse",
+    "hotvle hvse",
+    "tasacuce",
+    "tasace rakko",
+    "ke hvse",
+    "kvco hvse",
+    "hiyuce",
+    "hiyo rakko",
+    "otowoskuce",
+    "otowosko rakko",
+    "ehole",
+    "rvfo rakko",
+];
+
+const DAYS: [&str; 31] = [
+    "'svhvmkat",
+    "'svhokkolat",
+    "'svtuccēnat",
+    "'sostat",
+    "'svcahkepat",
+    "'svpakat",
+    "eskolvpakat",
+    "escenvpokakat",
+    "'sostvkakat",
+    "'svpalat",
+    "palen 'svhvmkat",
+    "palen 'svhokkolat",
+    "palen 'svtuccēnat",
+    "palen 'sostat",
+    "palen 'svcahkepat",
+    "palen 'svpakat",
+    "palen eskolvpakat",
+    "palen escenvpokakat",
+    "palen 'sostvkakat",
+    "pale 'svhokkolat",
+    "pale-hokkolen 'svhvmkat",
+    "pale-hokkolen 'svhokkolat",
+    "pale-hokkolen 'svtuccēnat",
+    "pale-hokkolen 'svostat",
+    "pale-hokkolen 'svcahkepat",
+    "pale-hokkolen 'svpakat",
+    "pale-hokkolen eskolvpakat",
+    "pale-hokkolen escenvpakat",
+    "pale-hokkolen 'sostvkakat",
+    "pale 'svtuccēnat",
+    "pale-tuccēnan 'svhvmkat",
+];
+
 pub fn respond(text: String) -> impl Responder {
     mk_respond(mk_response(text))
 }
@@ -33,78 +92,19 @@ fn mk_respond(response: Response) -> impl Responder {
 }
 
 pub fn number_to_day_of_month(day: u32) -> String {
-    let days = [
-        "'svhvmkat",
-        "'svhokkolat",
-        "'svtuccēnat",
-        "'sostat",
-        "'svcahkepat",
-        "'svpakat",
-        "eskolvpakat",
-        "escenvpokakat",
-        "'sostvkakat",
-        "'svpalat",
-        "palen 'svhvmkat",
-        "palen 'svhokkolat",
-        "palen 'svtuccēnat",
-        "palen 'sostat",
-        "palen 'svcahkepat",
-        "palen 'svpakat",
-        "palen eskolvpakat",
-        "palen escenvpokakat",
-        "palen 'sostvkakat",
-        "pale 'svhokkolat",
-        "pale-hokkolen 'svhvmkat",
-        "pale-hokkolen 'svhokkolat",
-        "pale-hokkolen 'svtuccēnat",
-        "pale-hokkolen 'svostat",
-        "pale-hokkolen 'svcahkepat",
-        "pale-hokkolen 'svpakat",
-        "pale-hokkolen eskolvpakat",
-        "pale-hokkolen escenvpakat",
-        "pale-hokkolen 'sostvkakat",
-        "pale 'svtuccēnat",
-        "pale-tuccēnan 'svhvmkat",
-    ];
-
-    let result = *days.get(day as usize).unwrap();
+    let result = *DAYS.get(day as usize).unwrap();
 
     result.to_string()
 }
 
 pub fn number_to_month(month: u32) -> String {
-    let months = [
-        "rvfocuse",
-        "hotvle hvse",
-        "tasacuce",
-        "tasace rakko",
-        "ke hvse",
-        "kvco hvse",
-        "hiyuce",
-        "hiyo rakko",
-        "otowoskuce",
-        "otowosko rakko",
-        "ehole",
-        "rvfo rakko",
-    ];
-
-    let result = *months.get(month as usize).unwrap();
+    let result = *MONTHS.get(month as usize).unwrap();
 
     result.to_string()
 }
 
 pub fn number_to_day_of_week(day: u32) -> String {
-    let days = [
-        "tacakuce",
-        "tvcakuce enhayvtke",
-        "mvnte enhayvtke",
-        "ennvrkvpv",
-        "ennvrkvpv enhayvtke",
-        "okkoskv nettv",
-        "tacakcuse",
-    ];
-
-    let result = *days.get(day as usize).unwrap();
+    let result = *DAYS_OF_WEEK.get(day as usize).unwrap();
 
     result.to_string()
 }
